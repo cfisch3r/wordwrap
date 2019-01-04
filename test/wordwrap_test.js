15bc8997912a98f77c0ab170aa2e1f4432c53be6
Christian Fischer <cf@fisch3r.de>
2019-01-04 10:46:23 +0100

    pass test for text within limit

--- test/wordwrap_test.js
+++ test/wordwrap_test.js
@@ -3,8 +3,13 @@ var wordrap = require('../');
 
 describe('wordwrap', function() {
     it('wraps empty string with no line break.', function() {
-      var wrappedText = wordrap('');
+      var wrappedText = wordrap('',4);
       wrappedText.should.equal('');
     });
 
-});
+    it('wraps text shorter within limit with no line break.', function() {
+      var wrappedText = wordrap('test',4);
+      wrappedText.should.equal('test');
+    });
+
+  });
