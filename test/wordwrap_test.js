74960a8d7bef089ce6290a95d4f4a42c4e2440b8
Christian Fischer <cf@fisch3r.de>
2019-01-04 11:56:20 +0100

    add remaining tests and complete function

--- test/wordwrap_test.js
+++ test/wordwrap_test.js
@@ -17,9 +17,33 @@ describe('wordwrap', function() {
       wrappedText.should.equal('test\ntest');
     });
 
-    it('breaks text with space after limit and skips space.', function() {
+    it('breaks text with space imediately after limit and skips space.', function() {
       var wrappedText = wordrap('test test',4);
       wrappedText.should.equal('test\ntest');
     });
 
+    it('breaks text with space within limit at space position and skips space.', function() {
+      var wrappedText = wordrap('te st',4);
+      wrappedText.should.equal('te\nst');
+    });
+
+    it('breaks text with multiple spaces within limit at last space position and skips space.', function() {
+      var wrappedText = wordrap('t e st',4);
+      wrappedText.should.equal('t e\nst');
+    });
+
+    it('breaks text with space after limit at limit', function() {
+      var wrappedText = wordrap('tests t',4);
+      wrappedText.should.equal('test\ns t');
+    });
+
+    it('breaks text with space within limit and after limit at space position within limit and skips space.', function() {
+      var wrappedText = wordrap('te st s',4);
+      wrappedText.should.equal('te\nst s');
+    });
+
+    it('breaks long text multiple times.', function() {
+      var wrappedText = wordrap('testtesttest',4);
+      wrappedText.should.equal('test\ntest\ntest');
+    });
   });
