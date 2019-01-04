1a96d37530f386a9f6187c1341e2d5f58cd99a93
Christian Fischer <cf@fisch3r.de>
2019-01-04 10:32:41 +0100

    pass test for empty string

--- test/wordwrap_test.js
+++ test/wordwrap_test.js
@@ -1,11 +1,9 @@
 var assert = require('assert');
-describe('Array', function() {
-    it('abc', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-
-    it('xyz', function() {
-      assert.equal([1,2,3].indexOf(3), 2);
+var wordrap = require('../');
+describe('wordwrap', function() {
+    it('wraps empty string with no line break.', function() {
+      var wrappedText = wordrap('');
+      assert.equal('', wrappedText);
     });
 
 });
